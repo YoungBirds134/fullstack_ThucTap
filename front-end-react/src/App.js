d5193bcbe49d7d27359227d1cb05720d55135de8
YoungBirds134 <Huynguyen2913@gmail.com>
2021-12-30 11:32:03 +0700

    - React FrontEnd

---
+++ front-end-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ScreenLogin from './screens/ScreenLogin';
+function App() {
+  return (
+    <div className="App">
+      <ScreenLogin/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end-react/src/screens/ScreenLogin/index.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import Login from './../../Components/Login/index'
+export default function ScreenLogin() {
+
+
+    return <>
+        <Login />
+
+    </>
+}
