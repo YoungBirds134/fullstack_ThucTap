63a5323109c938ea3fdbfaeeee3bc1c4e238f5dc
YoungBirds134 <Huynguyen2913@gmail.com>
2022-01-04 09:21:46 +0700

    - fix bug

--- coreui-free-react-admin-template-main/src/views/pages/login/Login.js
+++ coreui-free-react-admin-template-main/src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -12,26 +12,11 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
-} from "@coreui/react";
-import CIcon from "@coreui/icons-react";
-import { cilLockLocked, cilUser } from "@coreui/icons";
-import { useForm } from "react-hook-form";
-import axios from "axios";
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
 
-const Login = (...props) => {
-  // const [user, setUser] = useState([
-  //   {
-  //     username: "",
-  //     password: "",
-  //   },
-  // ]);
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  // } = useForm();
-  // const onSubmit = (data) => console.log(data);
+const Login = () => {
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -40,19 +25,14 @@ const Login = (...props) => {
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
-                  <CForm onSubmit={handleSubmit(onSubmit)}>
+                  <CForm>
                     <h1>Login</h1>
-                    <p className="text-medium-emphasis">
-                      Sign In to your account
-                    </p>
+                    <p className="text-medium-emphasis">Sign In to your account</p>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput
-                        placeholder="Username"
-                        autoComplete="username"
-                      />
+                      <CFormInput placeholder="Username" autoComplete="username" />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -66,11 +46,9 @@ const Login = (...props) => {
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <Link to="/home">
-                          <CButton color="primary" className="px-4">
-                            Login
-                          </CButton>
-                        </Link>
+                        <CButton color="primary" className="px-4">
+                          Login
+                        </CButton>
                       </CCol>
                       <CCol xs={6} className="text-right">
                         <CButton color="link" className="px-0">
@@ -81,25 +59,16 @@ const Login = (...props) => {
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard
-                className="text-white bg-primary py-5"
-                style={{ width: "44%" }}
-              >
+              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua.
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                     <Link to="/register">
-                      <CButton
-                        color="primary"
-                        className="mt-3"
-                        active
-                        tabIndex={-1}
-                      >
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
                         Register Now!
                       </CButton>
                     </Link>
@@ -111,7 +80,7 @@ const Login = (...props) => {
         </CRow>
       </CContainer>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
