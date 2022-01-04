15af7ba6e1e8eff6c352e79fed42b12de93240f6
YoungBirds134 <Huynguyen2913@gmail.com>
2022-01-04 08:29:23 +0700

    - remove metronic

--- front-end-coreui/src/routes.js
+++ front-end-coreui/src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react'
+const Login = React.lazy(() => import('./views/pages/login/Login'))
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
+  { path: '/', exact: true, name: 'Login' , component: Login },
   { path: '/home', name: 'Dashboard', component: Dashboard },
 ]
 
